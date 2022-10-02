3fa459ab9773b1d7254322effd350b79ab66ded3
levedoper <ricardoferegrinojr@gmail.com>
2022-10-02 00:56:50 -0500

    removed unused dependencies

--- src/services/apiAjax.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-unused-vars */
-import $ from 'jquery'
-import { useEffect, useState } from 'react'
-import * as satellite from 'satellite.js'
-
-export const ApiAjax = async () => {
-  const url = 'https://tle.ivanstanojevic.me/api/tle/25544'
-  return await fetch(url).then(response => response.json())
-}
