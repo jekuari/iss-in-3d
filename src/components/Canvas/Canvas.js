5cd3255092722e063d0407cf428411db396b050d
levedoper <ricardoferegrinojr@gmail.com>
2022-10-01 20:29:29 -0500

    added model with basic three

--- src/components/Canvas/Canvas.js
+++ src/components/Canvas/Canvas.js
@@ -2,50 +2,117 @@ import React, { useRef, useEffect } from 'react';
 
 import * as THREE from 'three';
 
-import { default as Earth } from '../Earth/Earth';
+import Stats from 'three/addons/libs/stats.module.js';
+
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { RoomEnvironment } from 'three/addons/environments/RoomEnvironment.js';
 
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
+
 
 export default function Canvas() {
 
    const container = useRef(null);
 
+   const clock = new THREE.Clock();
+
+
+   // const stats = new Stats();
+   // container.appendChild( stats.dom );
+
+   const renderer = new THREE.WebGLRenderer( { antialias: true } );
+   // renderer.setPixelRatio( window.devicePixelRatio );
+   renderer.setSize( window.innerWidth, window.innerHeight );
+   renderer.outputEncoding = THREE.sRGBEncoding;
+
+   const pmremGenerator = new THREE.PMREMGenerator( renderer );
+
    const scene = new THREE.Scene();
-   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01, 1000);
+   scene.background = new THREE.Color( 0x000224 );
+   scene.environment = pmremGenerator.fromScene( new RoomEnvironment(), 0.04 ).texture;
 
-   const renderer = new THREE.WebGLRenderer();
-   renderer.setSize(window.innerWidth, window.innerHeight);
-   document.body.appendChild(renderer.domElement);
+   const camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 100 );
+   camera.position.set( 5, 2, 8 );
 
-   const geometry = new THREE.BoxGeometry(1, 1, 1);
-   const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-   const cube = new THREE.Mesh(geometry, material);
-   scene.add(cube);
+   const controls = new OrbitControls( camera, renderer.domElement );
+   controls.target.set( 0, 0.5, 0 );
+   controls.update();
+   controls.enablePan = true;
+   controls.enableDamping = false;
+
+   const dracoLoader = new DRACOLoader();
+   dracoLoader.setDecoderPath( 'js/libs/draco/gltf/' );
+
+   const loader = new GLTFLoader();
+   loader.setDRACOLoader( dracoLoader );
+   loader.load( '/resources/earth.glb', function ( gltf ) {
+
+      const model = gltf.scene;
+      model.position.set( 0, 0, 0 );
+      model.scale.set( 0.005, 0.005, 0.005 );
+      scene.add( model );
+
+      // mixer = new THREE.AnimationMixer( model );
+      // mixer.clipAction( gltf.animations[ 0 ] ).play();
+
+      
+
+   }, undefined, function ( e ) {
+
+      console.error( e );
+
+   } );
+
+   loader.load( '/resources/iss.glb', function ( gltf ) {
+
+      const model = gltf.scene;
+      model.position.set( 20, 0, 0 );
+      model.scale.set( 0.1, 0.1, 0.1 );
+      scene.add( model );
+
+      // mixer = new THREE.AnimationMixer( model );
+      // mixer.clipAction( gltf.animations[ 0 ] ).play();
+
+      animate();
+
+   }, undefined, function ( e ) {
+
+      console.error( e );
+
+   } );
 
 
-   useEffect(() => {
-      loader.load('/resources/iss.glb', function (glb) {
+   // window.onresize = function () {
 
-         scene.add(glb.scene);
+   //    // camera.aspect = window.innerWidth / window.innerHeight;
+   //    // camera.updateProjectionMatrix();
 
-      }, undefined, function (error) {
-         alert(error);
-      });
-
-   }, [])
-
-   camera.position.z = 5;
-
-   renderer.render(scene, camera);
+   //    renderer.setSize( window.innerWidth, window.innerHeight );
+   //    renderer.setPixelRatio( window.devicePixelRatio );
+   // };
 
 
+   function animate() {
+
+      // requestAnimationFrame( animate );
+
+      // const delta = clock.getDelta();
+
+      // mixer.update( delta );
+
+      controls.update();
+
+      // stats.update();
+
+      renderer.render( scene, camera );
+
+   }
 
    useEffect(() => {
       container.current.innerHTML = "";
       container.current.append(renderer.domElement);
    }, [container, renderer.domElement]);
-
-   const loader = new GLTFLoader();
 
    return (
       <div ref={container} >
