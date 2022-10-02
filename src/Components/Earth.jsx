42187a6282211bb17f0280666b4eb164410818fa
levedoper <ricardoferegrinojr@gmail.com>
2022-10-02 01:00:14 -0500

    fnished basic position information

---
+++ src/Components/Earth.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useRef, useState } from 'react'
+
+
+const Earth = () => {
+  // const [position, setPosition] = useState()
+  
+
+  return (
+    <div>a</div>
+  )
+}
+
+export default Earth
